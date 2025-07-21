aeaa415a83e5bad3589bcb8ea8fd73d563bda06d
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-07-21 11:24:59 +0300

    feat: add mapper

---
+++ src/main/java/gr/aueb/cf/schoolapp2/mapper/Mapper.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.schoolapp2.mapper;
+
+import gr.aueb.cf.schoolapp2.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp2.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp2.model.Teacher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapper {
+
+    public Teacher mapToTeacherEntity(TeacherInsertDTO dto) {
+        return new Teacher(null, null, dto.getVat(), dto.getFirstname(), dto.getLastname(), null);
+    }
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO (Teacher teacher) {
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreatedAt(), teacher.getUpdatedAt(),
+                teacher.getFirstname(), teacher.getLastname(), teacher.getVat(), teacher.getRegion().getName());
+    }
+}
