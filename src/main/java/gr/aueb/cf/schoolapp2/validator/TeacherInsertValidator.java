8ec7b514891faf72747acc0b2676f64526ff4eeb
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-07-22 12:14:30 +0300

    feat: add validator

---
+++ src/main/java/gr/aueb/cf/schoolapp2/validator/TeacherInsertValidator.java
@@ -0,0 +1,44 @@
+package gr.aueb.cf.schoolapp2.validator;
+
+import gr.aueb.cf.schoolapp2.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp2.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp2.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp2.model.static_data.Region;
+import gr.aueb.cf.schoolapp2.repository.RegionRepository;
+import gr.aueb.cf.schoolapp2.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TeacherInsertValidator implements Validator {
+
+    private final TeacherRepository teacherRepository;
+    private final RegionRepository regionRepository;
+
+    @Override
+    public boolean supports(@NonNull Class<?> clazz) {
+        return TeacherInsertDTO.class == clazz;
+    }
+
+    @Override
+    public void validate(@NonNull Object target, Errors errors) {
+        TeacherInsertDTO teacherInsertDTO = (TeacherInsertDTO) target;
+
+        if (teacherRepository.findByVat(teacherInsertDTO.getVat()).isPresent()) {
+            log.error("Save failed for Teacher with vat={}. Teacher already exists", teacherInsertDTO.getVat());
+            errors.rejectValue("vat", "To ΑΦΜ του καθηγητή υπάρχει ήδη");
+
+        }
+
+        if (regionRepository.findById(teacherInsertDTO.getRegionId()).isEmpty()) {
+            log.error("Save failed for Teacher with vat={}. Region id={} invalid", teacherInsertDTO.getVat(), teacherInsertDTO.getRegionId());
+            errors.rejectValue("regionId", "Η περιοχή του καθηγητή δεν μπορεί να είναι κενή");
+        }
+    }
+}
