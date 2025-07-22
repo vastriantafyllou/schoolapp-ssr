35567d658972b5bd018e24f9cd11972131652b2b
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-07-22 12:32:01 +0300

    feat(service): add getPaginatedTeachers at ITeacherService

--- src/main/java/gr/aueb/cf/schoolapp2/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp2/service/ITeacherService.java
@@ -3,10 +3,14 @@ package gr.aueb.cf.schoolapp2.service;
 import gr.aueb.cf.schoolapp2.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp2.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp2.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp2.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp2.model.Teacher;
+import org.springframework.data.domain.Page;
 
 public interface ITeacherService {
+
     Teacher saveTeacher(TeacherInsertDTO teacherInsertDTO)
         throws EntityAlreadyExistsException, EntityInvalidArgumentException;
 
+    Page<TeacherReadOnlyDTO> getPaginatedTeachers(int page, int size);
 }
