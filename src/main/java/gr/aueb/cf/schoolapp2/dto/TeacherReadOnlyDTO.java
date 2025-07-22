8484c78f21e306cfd49553d1a5d1e760092ed018
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-07-22 11:41:27 +0300

    feat(dto): add TeacherReadOnlyDTO

---
+++ src/main/java/gr/aueb/cf/schoolapp2/dto/TeacherReadOnlyDTO.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.schoolapp2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TeacherReadOnlyDTO {
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String firstname;
+    private String lastname;
+    private String vat;
+    private String region;
+
+
+}
