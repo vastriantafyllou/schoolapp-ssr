cebe108d81adb343357834f1327c4f8b062fde4b
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-08-07 10:45:24 +0300

    feat: add security config

---
+++ src/main/java/gr/aueb/cf/schoolapp2/authentication/SecurityConfig.java
@@ -0,0 +1,50 @@
+package gr.aueb.cf.schoolapp2.authentication;
+
+import gr.aueb.cf.schoolapp2.core.enums.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "index.html").permitAll()
+                        .requestMatchers("/school/users/register").permitAll()
+                        .requestMatchers("/school/teachers/**").hasAnyAuthority(Role.TEACHER.name())
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+//                        .usernameParameter("username")
+//                        .passwordParameter("password")
+//                        .loginProcessingUrl("/login")
+//                        .failureUrl("/login?error")
+                        .defaultSuccessUrl("/school/teachers", true)
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+
+                return http.build();
+
+
+    }
+}
