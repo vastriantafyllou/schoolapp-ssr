3427bcedab3e7d83afca2591e7443de513413f5f
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-08-07 13:59:55 +0300

    feat: add mapper for user from user insert dto

--- src/main/java/gr/aueb/cf/schoolapp2/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp2/mapper/Mapper.java
@@ -1,9 +1,12 @@
 package gr.aueb.cf.schoolapp2.mapper;
 
+import gr.aueb.cf.schoolapp2.core.enums.Role;
 import gr.aueb.cf.schoolapp2.dto.TeacherEditDTO;
 import gr.aueb.cf.schoolapp2.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp2.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp2.dto.UserInsertDTO;
 import gr.aueb.cf.schoolapp2.model.Teacher;
+import gr.aueb.cf.schoolapp2.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,5 +23,10 @@ public class Mapper {
 
     public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher) {
         return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(), teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
+    }
+
+    public User mapToUserEntity(UserInsertDTO userInsertDTO) {
+        return new User(userInsertDTO.getUsername(), userInsertDTO.getPassword(),
+                Role.valueOf(userInsertDTO.getRole().toUpperCase()));
     }
 }
