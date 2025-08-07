e9c25d97cf879cd00f3a47636d4a8262c317d503
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-08-08 00:03:49 +0300

    feat(controller): add user save method for post mapping

---
+++ src/main/java/gr/aueb/cf/schoolapp2/controller/UserController.java
@@ -0,0 +1,50 @@
+package gr.aueb.cf.schoolapp2.controller;
+
+import gr.aueb.cf.schoolapp2.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp2.dto.UserInsertDTO;
+import gr.aueb.cf.schoolapp2.service.UserService;
+import gr.aueb.cf.schoolapp2.validator.UserInsertValidator;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/school")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final UserInsertValidator userInsertValidator;
+
+    @GetMapping("/users/register")
+    public String getUserForm(Model model) {
+        model.addAttribute("userInsertDTO", new UserInsertDTO());
+        return "user-form";
+    }
+
+    @PostMapping("/users/register")
+    public String insertUser(@Valid @ModelAttribute("userInsertDTO") UserInsertDTO userInsertDTO,
+                             BindingResult bindingResult, Model model) {
+
+        userInsertValidator.validate(userInsertDTO, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "user-form";
+        }
+
+        try {
+             userService.saveUser(userInsertDTO);
+            return "redirect:/";
+        } catch (EntityAlreadyExistsException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "user-form";
+        }
+    }
+}
