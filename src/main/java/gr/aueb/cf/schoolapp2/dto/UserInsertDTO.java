d4f6b650d857da29df72033528f495ca0fe590f8
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-08-08 00:41:28 +0300

    fix: add @NotNull to password to catch nulls

--- src/main/java/gr/aueb/cf/schoolapp2/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp2/dto/UserInsertDTO.java
@@ -18,6 +18,7 @@ public class UserInsertDTO {
     @Size(min = 2, max = 20, message = "Το username πρέπει να είναι μεταξύ 2 - 20 χαρακτήρες")
     private String username;
 
+    @NotNull(message = "To password δεν μπορεί να είναι null.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$",
             message = "Το password πρέπει να περιέχει τουλάχιστον 1 πεζό, 1 κεφαλαίο, 1 ψηφίο και 1 ειδικό χαρακτήρα χωρίς κενά.")
     private String password;
