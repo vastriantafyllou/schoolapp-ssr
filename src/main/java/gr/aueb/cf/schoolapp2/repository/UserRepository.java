46dec63eda9fb53f11600d4502df3156c1f41da8
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-08-09 10:56:13 +0300

    fix: change enum role to entity role

--- src/main/java/gr/aueb/cf/schoolapp2/repository/UserRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp2/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package gr.aueb.cf.schoolapp2.repository;
 
-import gr.aueb.cf.schoolapp2.core.enums.Role;
 import gr.aueb.cf.schoolapp2.model.User;
+import gr.aueb.cf.schoolapp2.model.auth.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
