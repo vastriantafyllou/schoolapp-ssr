5ca0e450ac0749eb589c3085e51333f7baa25545
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-08-09 10:55:23 +0300

    feat: add repositories for role and capabilities

---
+++ src/main/java/gr/aueb/cf/schoolapp2/repository/CapabilityRepository.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.schoolapp2.repository;
+
+import gr.aueb.cf.schoolapp2.model.auth.Capability;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CapabilityRepository extends JpaRepository<Capability, Long> {
+}
